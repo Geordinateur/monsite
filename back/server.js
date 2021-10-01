8039dd5dca12864f6d555d8ffbe38bb759bdfba4
Geordinateur <emuricardo@gmail.com>
2021-10-01 13:40:59 +0200

    Starting

---
+++ back/server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const app = require("./API/app.js");
+const dotenv = require("dotenv");
+dotenv.config({ path: __dirname + '\\.env' })
+
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+
+server.on('listening', function(){
+    console.log(`listening on http://localhost:${port}`);
+})
+
+server.listen(port);
